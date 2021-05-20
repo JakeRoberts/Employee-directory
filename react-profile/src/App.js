9fc223cc9475df0aa33524e4c95c1f1b58cdf3ad
Rob <r.jacobs790@gmail.com>
2021-05-20 16:13:24 -0400

    installed react app and created files in componets and style

---
+++ react-profile/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Wrapper from './components/Wrapper';
+import Header from './components/Header';
+import Main from './components/Main'
+
+function App() {
+  return (
+    <Wrapper>
+      <Header />
+      <Main />
+    </Wrapper>
+  );
+}
+
+export default App;
